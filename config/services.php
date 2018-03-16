f40b219882b313e24f2494e01af2da4dbf2ccf01
phucluu <phucluuftu@gmail.com>
2018-03-16 16:26:34 +0700

    Initial commit - Successfully login with Gmail Todo: + Check user in DB, finish auth

---
+++ config/services.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => 'us-east-1',
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
+    'google' => [
+        'client_id' => '228981541877-eu1up7ttd5f96e14qm95963klt8185j9.apps.googleusercontent.com',
+        'client_secret' => 'kQrb5QMnusbva5FkdKRq3SvK',
+        'redirect' => 'http://localhost:8000/auth/google/callback',
+    ],
+
+];
